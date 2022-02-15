fbd699c94e6c00a08eb514229568db095a12d7c8
Damir1975 <dkahvic@hotmail.com>
2022-02-15 13:50:31 +0100

    Css stor skärm klar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>OUR WEBSHOP</h1>
         <Intro />
       </div>
     </div>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,19 +1,24 @@
 import React from "react";
-import image1 from "../images/mockup 2.png"
-import image2 from "../images/IMAGE.png"
-import image3 from "../images/IMAGE (1).png"
-import image4 from "../images/IMAGE (2).png"
-import image5 from "../images/IMAGE (3).png"
+import image1 from "../images/mockup 2.png";
+import image2 from "../images/IMAGE.png";
+import image3 from "../images/IMAGE (1).png";
+import image4 from "../images/IMAGE (2).png";
+import image5 from "../images/IMAGE (3).png";
+import "./Intro.css";
 
-function Intro () {
-    return (
-        <div className="intro">
-            <img id="picture1" src={image1}></img>
-            <img id="picture2" src={image2}></img>
-            <img id="picture3" src={image3}></img> 
-            <img id="picture4" src={image4}></img> 
-            <img id="picture5" src={image5}></img>             
-        </div>
-    )    
+function Intro() {
+  return (
+    <div className="intro">
+      <img id="picture1" src={image1}></img>
+      <div className="intro2">
+        <img id="picture2" src={image2}></img>
+        <img id="picture4" src={image4}></img>
+      </div>
+      <div className="intro3">
+        <img id="picture3" src={image3}></img>
+        <img id="picture5" src={image5}></img>
+      </div>
+    </div>
+  );
 }
 export default Intro;

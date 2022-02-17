1d18c199340e253807d983917480f26e62bf6597
Carl-Lander <callelander98@gmail.com>
2022-02-17 10:46:08 +0100

    new

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Intro from "./components/Intro.js";
 import Button from "./components/Button.js";
 import SizeButton from "./components/SizeButton.js";
+import Header from "./components/Header.js";
 
 function App() {
   return (
     <div className="App">
       <div>
+      <Header></Header>
         <Intro />
         <Button>Köp</Button>
         <SizeButton>S</SizeButton>

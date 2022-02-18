6629c58587ebcbaa60c1157ba827e52022a5061c
Carl-Lander <92018685+Carl-Lander@users.noreply.github.com>
2022-02-18 11:14:34 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from 'react';
 import "./App.css";
 import Intro from "./components/Intro.js";
 import Button from "./components/Button.js";
 import SizeButton from "./components/SizeButton.js";
+import Header from "./components/Header.js";
+import Shipping from './components/Shipping.js';
 
 function App() {
   return (
     <div className="App">
       <div>
+        <Header></Header>
         <Intro />
+        <Shipping></Shipping>
         <Button>Köp</Button>
         <SizeButton>S</SizeButton>
       </div>
---
+++ src/components/Shipping.js
@@ -0,0 +1,21 @@
+import React from "react";
+import rectangle1 from "../images/Rectangle (1).svg"; 
+import rectangle2 from "../images/Rectangle (2).svg";
+import rectangle3 from "../images/Rectangle (3).svg";
+import rectangle from "../images/Rectangle.svg";
+import "./Shipping.css";
+
+
+
+function Shipping(){
+    return(
+        <div className="shipping">
+            <div><img className="truck__logo" src={rectangle} /><span>  Snabb leverans</span></div>
+            <div><img className="box__logo" src={rectangle1}/><span>  Fri frakt över 500 kr</span></div>
+            <div><img className="pin__logo" src={rectangle2}/><span>  Klubben för bästa vänner</span></div>
+            <div><img className="chat__logo" src={rectangle3}/><span>  Professionell service</span></div>
+        </div>
+    );
+}
+
+export default Shipping;

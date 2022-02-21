d3f6fb14c02f727909337df35a6bd4f05d914830
Carl-Lander <92018685+Carl-Lander@users.noreply.github.com>
2022-02-21 10:27:50 +0100

    klar version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import React from "react";
 import "./App.css";
 import Intro from "./components/Intro.js";
 import { useState } from "react";
 import Products from "./components/Products";
 import { propTypes } from "react-bootstrap/esm/Image";
+import Button from "./components/Button.js";
+import SizeButton from "./components/SizeButton.js";
+import Header from "./components/Header.js";
+import Shipping from './components/Shipping.js';
 
 function App() {
   
   return (
-    <div>
+    <div className="App">
+      <div>
+        <Header></Header>
+        <Intro />
+        <Shipping></Shipping>
     <Products catId={1}></Products>
     <Products catId={2}></Products>
     <Products catId={3}></Products>
     </ div>
+    </div>
   );
 };
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import Button from "./Button.js";
 import "./Item.css";
 

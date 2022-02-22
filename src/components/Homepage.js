3f6486af335523aa11db08bcceaff7287fbaf47f
Will7e <91021939+Will7e@users.noreply.github.com>
2022-02-22 09:36:31 +0100

    Homepage update

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import Products from "./Products";
+import Intro from "./Intro";
 import "./Home.css";
 
 const Homepage = () => {
   return (
     <div className="home">
-      <div className="home__container"></div>
+      <div className="home__container">
+        <Intro />
+        <Products catId={1}></Products>
+        <Products catId={2}></Products>
+        <Products catId={3}></Products>
+      </div>
     </div>
   );
 };
---
+++ src/components/ProductsList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductsList = (props) => {
+  return <div>Hund</div>;
+};
+
+export default ProductsList;

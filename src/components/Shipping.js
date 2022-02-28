0e3e3847ff037b710decec585d79f574625e307a
Will7e <91021939+Will7e@users.noreply.github.com>
2022-02-28 17:07:58 +0100

    media 428px homepage

--- src/components/Shipping.js
+++ src/components/Shipping.js
@@ -4,38 +4,37 @@ import "./Shipping.css";
 function Shipping() {
   return (
     <div className="shipping">
-      <div className="wrapperr">
-        <div className="container">
-          <img className="truck__logo" src="../images/Rectangle.svg" />
-
-          <div className="container__text">
-            <p> Snabb leverans</p>
-            <p>Lorem ipsum</p>
-          </div>
+      <div className="wrapping">
+        <div className="box-holder">
+          <img
+            className="shipping__logo truck-logo"
+            src="../images/Rectangle.svg"
+          ></img>
+          <span className="shipping__text">
+            Snabb leverans <span>Du får din beställning snabbt</span>
+          </span>
         </div>
 
-        <div className="container">
-          <img className="box__logo" src="../images/Rectangle (1).svg" />
-          <div className="container__text">
-            <p> Snabb leverans</p>
-            <p>Lorem ipsum</p>
-          </div>
+        <div className="box-holder">
+          <img className="shipping__logo " src="../images/Rectangle (1).svg" />
+          <span className="shipping__text">
+            Fri frakt vid beställning över 500kr{" "}
+            <span>Endast 49kr om mindre</span>
+          </span>
         </div>
 
-        <div className="container">
-          <img className="pin__logo" src="../images/Rectangle (2).svg" />
-          <div className="container__text">
-            <p> Snabb leverans</p>
-            <p>Lorem ipsum</p>
-          </div>
+        <div className="box-holder">
+          <img className="shipping__logo" src="../images/Rectangle (2).svg" />
+          <span className="shipping__text">
+            Kundklubben <span>För bästa vänner</span>
+          </span>
         </div>
 
-        <div className="container">
-          <img className="chat__logo" src="../images/Rectangle (3).svg" />
-          <div className="container__text">
-            <p> Snabb leverans</p>
-            <p>Lorem ipsum</p>
-          </div>
+        <div className="box-holder">
+          <img className="shipping__logo" src="../images/Rectangle (3).svg" />
+          <span className="shipping__text">
+            Professionell service<span>Vi finns alltid här för dig</span>
+          </span>
         </div>
       </div>
     </div>
